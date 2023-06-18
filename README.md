a18583efee6a40bb3041aa3c5aca6341f6d3bb82
thanatkat <thanatkat@gmail.com>
2023-06-18 18:36:29 +0700

    Init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Test-Git
+Test Git hub
+with me!
---
+++ test.py
@@ -0,0 +1,3 @@
+# add -> commit -> push
+# staged changes บันทึกการเปลี่ยนแปลง ก่อนจะ push ขึ้นไป บน Github
+print("Hello World, I'm python developer")
